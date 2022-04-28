62486b795c2ec4ab8b0aa4340061a41ba6076a49
Meet Goswami <meet.goswami@iiitb.ac.in>
2022-04-28 20:46:30 +0530

    added log statements and test cases

--- spe_majorProject/src/main/java/com/example/spe_majorproject/controller/ElectiveController.java
+++ spe_majorProject/src/main/java/com/example/spe_majorproject/controller/ElectiveController.java
@@ -8,6 +8,8 @@ import com.example.spe_majorproject.repository.ApplicationRepository;
 import com.example.spe_majorproject.repository.ElectiveRepository;
 import com.example.spe_majorproject.repository.FacultyRepository;
 import com.example.spe_majorproject.repository.StudentRepository;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,11 +35,13 @@ public class ElectiveController {
   @Autowired
   private StudentRepository studentRepository;
 
+  private final Logger logger= LogManager.getLogger(ElectiveController.class);
+
   @GetMapping("/ProjectElectives")
   public ResponseEntity<List<Elective>> getProjectElectives()
   {
+    logger.info("[PROJECT_ELECTIVES] - GET ALL");
     List<Elective> projectElectives=electiverepo.findByType("Project Elective");
-
     return ResponseEntity.ok().header("Content-Type", "application/json")
             .body(projectElectives);
   }
@@ -45,6 +49,7 @@ public class ElectiveController {
   @GetMapping("/ResearchElectives")
   public ResponseEntity<List<Elective>> getResearchElectives()
   {
+    logger.info("[RESEARCH_ELECTIVES] - GET ALL");
     List<Elective> researchElectives=electiverepo.findByType("Research Elective");
     return ResponseEntity.ok().header("Content-Type", "application/json")
             .body(researchElectives);
@@ -59,6 +64,7 @@ public class ElectiveController {
   @PostMapping("/ProjectElectives/apply")
   public ResponseEntity<Response> applyForPE(@RequestBody Application application)
   {
+    logger.info("[PROJECT_ELECTIVES] - APPLY");
     Response response=new Response();
     Student student=studentRepository.findById(application.getEmail()).orElse(new Student());
     Elective elective=electiverepo.findById(application.getEid()).orElse(new Elective());
@@ -70,6 +76,7 @@ public class ElectiveController {
     {
       response.setStatus("Failed");
       response.setMessage("Application submission failed because you have already applied to this elective");
+      logger.info("[PROJECT_ELECTIVES APPLY] - "+response.getStatus());
       return ResponseEntity.badRequest().header("Content-Type", "application/json")
               .body(response);
     }
@@ -83,11 +90,13 @@ public class ElectiveController {
       apprepo.save(application);
       response.setStatus("Success");
       response.setMessage("Application submitted successfully");
+      logger.info("[PROJECT_ELECTIVES APPLY] - "+response.getStatus());
       return ResponseEntity.ok().header("Content-Type", "application/json")
               .body(response);
     }
     response.setStatus("Failed");
     response.setMessage("Application submission failed");
+    logger.info("[PROJECT_ELECTIVES APPLY] - "+response.getStatus());
     return ResponseEntity.badRequest().header("Content-Type", "application/json")
             .body(response);
   }
@@ -95,6 +104,7 @@ public class ElectiveController {
   @PostMapping("/ResearchElectives/apply")
   public ResponseEntity<Response> applyForRE(@RequestBody Application application)
   {
+    logger.info("[RESEARCH_ELECTIVES] - APPLY");
     Response response=new Response();
     Student student=studentRepository.findById(application.getEmail()).orElse(new Student());
     Elective elective=electiverepo.findById(application.getEid()).orElse(new Elective());
@@ -108,6 +118,7 @@ public class ElectiveController {
     {
       response.setStatus("Failed");
       response.setMessage("Application submission failed because you have already applied to this elective");
+      logger.info("[RESEARCH_ELECTIVES APPLY] - "+response.getStatus());
       return ResponseEntity.badRequest().header("Content-Type", "application/json")
               .body(response);
     }
@@ -122,23 +133,27 @@ public class ElectiveController {
       apprepo.save(application);
       response.setStatus("Success");
       response.setMessage("Application submitted successfully");
+      logger.info("[RESEARCH_ELECTIVES APPLY] - "+response.getStatus());
       return ResponseEntity.ok().header("Content-Type", "application/json")
               .body(response);
     }
     response.setStatus("Failed");
     response.setMessage("Application submission failed");
+    logger.info("[RESEARCH_ELECTIVES APPLY] - "+response.getStatus());
     return ResponseEntity.badRequest().header("Content-Type", "application/json")
             .body(response);
   }
   @PostMapping ("/MyElectives")
   public ResponseEntity<List<Elective>> getElectives(@RequestBody Map<String,String> factEmail)
   {
+    logger.info("[GET_MY_ELECTIVES] - "+factEmail.get("email"));
     return ResponseEntity.ok().header("Content-Type", "application/json")
             .body(electiverepo.findByEmail(factEmail.get("email")));
   }
   @PostMapping("/MyElectives/Add")
   public ResponseEntity<Response> addElective(@RequestBody Elective elective)
   {
+    logger.info("[ADD_MY_ELECTIVES] - "+elective.getEmail());
     Response response=new Response();
     Faculty factname=factrepo.findById(elective.getEmail()).orElse(new Faculty());
     if(!factname.equals("none"))
@@ -155,11 +170,13 @@ public class ElectiveController {
       electiverepo.save(elective);
       response.setStatus("Success");
       response.setMessage("New Elective added successfully");
+      logger.info("[ADD_MY_ELECTIVES - STATUS] - "+response.getStatus());
       return ResponseEntity.ok().header("Content-Type", "application/json")
               .body(response);
     }
     response.setStatus("Failed");
     response.setMessage("Failed to add new elective");
+    logger.info("[ADD_MY_ELECTIVES - STATUS] - "+response.getStatus());
     return ResponseEntity.badRequest().header("Content-Type", "application/json")
             .body(response);
   }
@@ -168,6 +185,7 @@ public class ElectiveController {
   @PostMapping("/MyElectives/Update")
   public ResponseEntity<Response> updateElective(@RequestBody Elective elective)
   {
+    logger.info("[UPDATE_MY_ELECTIVES] - "+elective.getEmail());
     Response response=new Response();
     Faculty factname=factrepo.findById(elective.getEmail()).orElse(new Faculty());
     if(!factname.equals("none"))
@@ -176,17 +194,20 @@ public class ElectiveController {
       electiverepo.save(elective);
       response.setStatus("Success");
       response.setMessage("Elective details updated successfully");
+      logger.info("[UPDATE_MY_ELECTIVES - STATUS] - "+response.getStatus());
       return ResponseEntity.ok().header("Content-Type", "application/json")
               .body(response);
     }
     response.setStatus("Failed");
     response.setMessage("Elective details not updated");
+    logger.info("[UPDATE_MY_ELECTIVES - STATUS] - "+response.getStatus());
     return ResponseEntity.badRequest().header("Content-Type", "application/json")
             .body(response);
   }
   @PostMapping("/MyApplications")
   public List<Application> getStudentApplications(@RequestBody Map<String ,String> studEmail)
   {
+    logger.info("[APPLICATIONS BY STUDENT] - "+studEmail.get("studentemail"));
     return apprepo.findByEmail(studEmail.get("studentemail"));
   }
 
@@ -194,6 +215,7 @@ public class ElectiveController {
   public List<Application> getApplications(@RequestBody Map<String,String> factEmail)
   {
     String status="Pending";
+    logger.info("[APPLICATIONS FOR FACULTY] - "+factEmail.get("facultyemail"));
     return apprepo.findByFacultyemailAndStatus(factEmail.get("facultyemail"),status);
   }
 
@@ -201,6 +223,7 @@ public class ElectiveController {
   @PostMapping("/Applications/Accept")
   public ResponseEntity<Response> acceptApplication(@RequestBody Map<String,Integer> application)
   {
+    logger.info("[ACCEPT APPLICATION]");
     Response response=new Response();
     int aid=application.get("aid");
     int eid=application.get("eid");
@@ -210,6 +233,7 @@ public class ElectiveController {
     {
       response.setStatus("Failed");
       response.setMessage("Invalid student id");
+      logger.info("[ACCEPT APPLICATION - STATUS] - "+response.getStatus());
       return ResponseEntity.badRequest().header("Content-Type", "application/json")
               .body(response);
     }
@@ -217,6 +241,7 @@ public class ElectiveController {
     {
       response.setStatus("Failed");
       response.setMessage("Cannot accept the application because either it is already accepted or rejected");
+      logger.info("[ACCEPT APPLICATION - STATUS] - "+response.getStatus());
       return ResponseEntity.badRequest().header("Content-Type", "application/json")
               .body(response);
     }
@@ -224,6 +249,7 @@ public class ElectiveController {
     {
       response.setStatus("Failed");
       response.setMessage("Invalid faculty id");
+      logger.info("[ACCEPT APPLICATION - STATUS] - "+response.getStatus());
       return ResponseEntity.badRequest().header("Content-Type", "application/json")
               .body(response);
     }
@@ -231,6 +257,7 @@ public class ElectiveController {
     {
       response.setStatus("Failed");
       response.setMessage("No vacancy available in this elective");
+      logger.info("[ACCEPT APPLICATION - STATUS] - "+response.getStatus());
       return ResponseEntity.badRequest().header("Content-Type", "application/json")
               .body(response);
     }
@@ -240,6 +267,7 @@ public class ElectiveController {
     electiverepo.save(elec);
     response.setStatus("Success");
     response.setMessage("Application accepted successfully");
+    logger.info("[ACCEPT APPLICATION - STATUS] - "+response.getStatus());
     return ResponseEntity.ok().header("Content-Type", "application/json")
             .body(response);
   }
@@ -247,6 +275,7 @@ public class ElectiveController {
   @PostMapping("/Applications/Reject")
   public ResponseEntity<Response> rejectApplication(@RequestBody Map<String,Integer> application)
   {
+    logger.info("[REJECT APPLICATION]");
     Response response=new Response();
     int aid=application.get("aid");
     Application appl=apprepo.findById(aid).orElse(new Application());
@@ -254,6 +283,7 @@ public class ElectiveController {
     {
       response.setStatus("Failed");
       response.setMessage("Invalid student id");
+      logger.info("[REJECT APPLICATION - STATUS] - "+response.getStatus());
       return ResponseEntity.badRequest().header("Content-Type", "application/json")
               .body(response);
     }
@@ -262,6 +292,7 @@ public class ElectiveController {
     apprepo.save(appl);
     response.setStatus("Success");
     response.setMessage("Application rejected successfully");
+    logger.info("[REJECT APPLICATION - STATUS] - "+response.getStatus());
     return ResponseEntity.ok().header("Content-Type", "application/json")
             .body(response);
   }
@@ -269,6 +300,7 @@ public class ElectiveController {
   @GetMapping("/Faculty")
   public List<Faculty> getFaculty()
   {
+    logger.info("[GET FACULTY LIST]");
     return factrepo.findAll();
   }
 }
--- spe_majorProject/src/test/java/com/example/spe_majorproject/controller/ElectiveControllerTest.java
+++ spe_majorProject/src/test/java/com/example/spe_majorproject/controller/ElectiveControllerTest.java
@@ -1,19 +1,34 @@
 package com.example.spe_majorproject.controller;
 
+import com.example.spe_majorproject.bean.Elective;
 import com.example.spe_majorproject.repository.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.hamcrest.Matchers;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.jsf.FacesContextUtils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-class ElectiveControllerTest {
+public class ElectiveControllerTest {
 
     private MockMvc mockMvc;
 
@@ -36,7 +51,38 @@ class ElectiveControllerTest {
     private ElectiveController electiveController;
 
     @Before
-    void setUp() {
+    public void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(electiveController).build();
+    }
+
+    ObjectMapper objectMapper=new ObjectMapper();
+
+    @Test
+    public void testGetProjectElectives() throws Exception
+    {
+        Elective elective1=new Elective(1,"Elective 1","Test Elective",10,"Test faculty","test@test.com","Project Elective");
+        Elective elective2=new Elective(2,"Elective 2","Test Elective",20,"Test faculty","test@test.com","Project Elective");
+
+        List<Elective> projctElectiveList=new ArrayList<>();
+        projctElectiveList.add(elective1);
+        projctElectiveList.add(elective2);
+
+//        JSONArray jsonArray=new JSONArray(projctElectiveList);
+//        String projectElectivesJson=objectMapper.writeValueAsString(projctElectiveList);
+//        JSONObject json = new JSONObject(projectElectivesJson);
+//        System.out.println(json.toString());
+
+        String json = objectMapper.writeValueAsString(projctElectiveList);
+        System.out.println(json);
+        JSONArray jsonArray=new JSONArray(json);
+        System.out.println(jsonArray);
+
+
+        when(electiveRepository.findByType("Project Elective")).thenReturn(projctElectiveList);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/dashboard/ProjectElectives"))
+                .andExpect(status().isOk());
+
 
     }
 }
